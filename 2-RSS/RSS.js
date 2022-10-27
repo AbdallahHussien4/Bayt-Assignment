eb7b04982c64d403ef332baf9c242097e1b007fd
AbdallahHussien4 <Abdalah.Abdelhameed98@eng-st.cu.edu.eg>
2022-10-28 00:30:32 +0200

    added google maps API functionalities

--- 2-RSS/RSS.js
+++ 2-RSS/RSS.js
@@ -1,18 +1,19 @@
-function buildTable(xmlDoc) {
+APIKEY = "AIzaSyBrxMji_hGo6bc2iuw7xHV80SDlV0EjUZY"
+function buildTable(jobs) {
     var table =
         `<tr><th>Title</th>
             <th>Country</th>
             <th>City</th>
+            <th>Location in Map</th>
         </tr>`;
-    var jobs = xmlDoc.getElementsByTagName("item");
-
     for (var i = 0; i < jobs.length; i++) {
         table += "<tr><td>" +
-        jobs[i].childNodes[1].textContent +         // Title
+        jobs[i].childNodes[1].textContent +                     // Title
         "</td><td>" +
-        jobs[i].childNodes[13].textContent +         //Country
+        jobs[i].childNodes[13].textContent +                    //Country
         "</td><td>" +
-        jobs[i].childNodes[12].textContent;         //City
+        jobs[i].childNodes[12].textContent+                     //City
+        "</td><td>" + "<button class='MapButton' onClick = \"updateFrame('"+jobs[i].childNodes[12].textContent+ "')\"> View in Map</button>";          //Loccation
     }
 
     // Print the xml data in table form
@@ -27,7 +28,8 @@ function run()
         if (this.readyState == 4 && this.status == 200) {
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(this.responseText,"text/xml");
-            buildTable(xmlDoc);
+            var jobs = xmlDoc.getElementsByTagName("item");
+            buildTable(jobs);
             // console.log(xmlDoc.getElementsByTagName("item")[1].childNodes[1].textContent); // Title
             // console.log(xmlDoc.getElementsByTagName("item")[1].childNodes[13].textContent); //Country
             // console.log(xmlDoc.getElementsByTagName("item")[1].childNodes[12].textContent); //City
@@ -35,4 +37,9 @@ function run()
     };
     xmlhttp.send(null);
 
+}
+
+function updateFrame(city)
+{
+  document.getElementById("map").src = "https://www.google.com/maps/embed/v1/place?key="+APIKEY+"&q="+city;
 }
